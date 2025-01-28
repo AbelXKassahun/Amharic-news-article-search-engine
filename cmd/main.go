f924e0dca2d51fc368c18bd381084f4ff55bbd65
AbelXKassahun <abelkassahun101@gmail.com>
2025-01-28 03:27:37 +0300

    fixed the scraper

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	// "fmt"
+	// "strings"
 	"search_engine/web_scraper"
 )
 
 func main() {
 	techSectionPageUrl := "https://www.bbc.com/amharic/topics/c06gq8wx467t?page="
 
-	web_scraper.ScrapeArticleUrls(techSectionPageUrl, 1) // 22
+	web_scraper.ScrapeArticleUrls(techSectionPageUrl, 22, "../corpus/tech_articles") // 22
+	// fmt.Println(strings.Split("https://www.bbc.com/amharic/articles/cj0ry2jgzjpo", "/")[5])
 }
